d1f7f60a44fb8c1fc5830bd5993aa4f4ce05d206
swordday <swordday2008@gmail.com>
2018-04-16 16:12:19 +0800

    set NDK Path

--- build_arm.sh
+++ build_arm.sh
@@ -1,7 +1,7 @@
 cd luajit-2.1/src
 
 # Android/ARM, armeabi-v7a (ARMv7 VFP), Android 4.0+ (ICS)
-NDK=D:/android-ndk-r10e
+NDK=C:/Android/android-ndk-r13b
 NDKABI=19
 NDKVER=$NDK/toolchains/arm-linux-androideabi-4.9
 NDKP=$NDKVER/prebuilt/windows-x86_64/bin/arm-linux-androideabi-
